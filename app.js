015662bc8cdb8c01d54511049ac7c79e2c77a5ef
Miles Morales <milesmorals2023@gmail.com>
2023-07-18 20:04:08 +0500

    The last one 9

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let c2 = document.querySelector(".clothes2")
 let c3 = document.querySelector(".clothes3")
 let c4 = document.querySelector(".clothes4")
 let c5 = document.querySelector(".clothes5")
+let con = document.querySelector(".container")
 
 function Func(){
     let val = input.value
@@ -14,30 +15,35 @@ function Func(){
         c3.style.display = "none"
         c4.style.display = "none"
         c5.style.display = "none"
+        con.style.height = "auto"
     } else if(val >= 0 && val <= 10){
         c1.style.display = "none"
         c2.style.display = "block"
         c3.style.display = "none"
         c4.style.display = "none"
         c5.style.display = "none"
+        con.style.height = "auto"
     } else if(val >= 10 && val <= 20){
         c1.style.display = "none"
         c2.style.display = "none"
         c3.style.display = "block"
         c4.style.display = "none"
         c5.style.display = "none"
+        con.style.height = "auto"
     } else if(val >= 20 && val <= 30){
         c1.style.display = "none"
         c2.style.display = "none"
         c3.style.display = "none"
         c4.style.display = "block"
         c5.style.display = "none"
+        con.style.height = "auto"
     } else if(val >= 30){
         c1.style.display = "none"
         c2.style.display = "none"
         c3.style.display = "none"
         c4.style.display = "none"
         c5.style.display = "block"
+        con.style.height = "auto"
     } else {
         c1.style.display = "none"
         c2.style.display = "none"
